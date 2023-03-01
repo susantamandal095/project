a69ecaffa149995b89979698773ec2e59e4063da
@susantamandal095 <susantamandal095@gmail.com>
2023-03-01 14:10:01 +0530

    diricative added

---
+++ test/src/app/shared/directive/allow-number-only.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AllowNumberOnlyDirective } from './allow-number-only.directive';
+
+describe('AllowNumberOnlyDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AllowNumberOnlyDirective(null);
+    expect(directive).toBeTruthy();
+  });
+});
